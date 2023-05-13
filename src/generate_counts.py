dcaa3145838eee9cda9337f6ac2611b6b1203252
Lauren Flynn <flynnlauren7@gmail.com>
2023-05-13 16:06:58 -0400

    added state codes and combined files

--- src/generate_counts.py
+++ src/generate_counts.py
@@ -32,7 +32,7 @@ connection = pg.connect(
 
 def get_outcomes():
     """ Get and return ICD codes """""
-    f = open('../icd_codes.json')
+    f = open('icd_codes.json')
     outcomes_ = json.load(f)
     f.close()
     return json.loads(outcomes_[0])
@@ -90,7 +90,7 @@ for key in diagnoses:
         print(sql_query)
         print("***************************************")
         data = pd.read_sql_query(sql_query, connection)
-        filename = f"../data/state/{key}_diagnoses_per_county_per_day_{year}_state.csv"
+        filename = f"data/state/{key}_diagnoses_per_county_per_day_{year}_state.csv"
         data.to_csv(filename)
         
         
